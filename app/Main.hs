3c96de5c8dc0e3be79555cdd204bad328a52877a
GiovanaJensen <oliveirasgiovana143@gmail.com>
2023-08-06 18:07:59 -0300

    learning lists

--- app/Main.hs
+++ app/Main.hs
@@ -2,3 +2,12 @@ module Main where
 
 main :: IO ()
 main = putStrLn "Hello, Haskell!"
+
+maiorQue :: Int -> Int -> Bool
+maiorQue x y = x > y
+
+dobroLista :: [Int] -> [Int]
+dobroLista xs = [2*x | x<-xs]
+
+lista :: [Int] 
+lista = [2*x+1 | x<-[0 .. 10], x/=5]
